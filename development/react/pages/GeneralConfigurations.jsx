089d24106db2f54fec4f5589eae3d9fdc80a0f66
Raul Canul <eduardoct120@gmail.com>
2018-03-20 09:51:12 -0600

    Left sider created

--- development/react/pages/GeneralConfigurations.jsx
+++ development/react/pages/GeneralConfigurations.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import { Layout, Menu, Icon, Avatar, Divider, Select, Modal, Tooltip, Dropdown } from 'antd'
+import './styles/configurations.css'
+
+const { Header, Sider, Content } = Layout
+const SubMenu = Menu.SubMenu
 
 class Generalconfigurations extends Component {
   constructor( props ) {
@@ -14,9 +18,74 @@ class Generalconfigurations extends Component {
 
   render() {
     return(
-      <div>
-        Hola
-      </div>
+      <Layout className="dashboard-layout">
+        <Sider
+          trigger = {null}
+          breakpoint = "lg"
+          width = {256}
+          collapsible collapsed={ this.state.collapsed }
+          className = { this.state.collapsed ? `sider drawer drawer-hide` : `sider drawer drawer-expanded` }
+        >
+
+          <div className="logo">
+            <h1>Dashboard</h1>
+          </div>
+
+          <Menu
+            theme="dark" 
+            mode="inline" 
+            style={{ padding: '16px 0', width: '100%' }} 
+            defaultSelectedKeys={['2']}
+            defaultOpenKeys={['sub2']}
+          >
+            <Menu.Item key="1">
+              <Icon type="dashboard" />
+              <span>Dashboard</span>
+            </Menu.Item>
+
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="setting" /><span>Configuración General</span></span>}
+            >
+              <Menu.Item key="2">Fichas</Menu.Item>
+              <Menu.Item key="3">Montos rápidos</Menu.Item>
+              <Menu.Item key="4">Tipo de cambio</Menu.Item>
+              <Menu.Item key="5">Precio de Membresía</Menu.Item>
+              <Menu.Item key="6">Horarios</Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub3"
+              title={<span><Icon type="idcard" /><span>Gestión de Roles</span></span>}
+            >
+              <Menu.Item key="7">Option sub 3</Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub4"
+              title={<span><Icon type="team" /><span>Administración de Usuarios</span></span>}
+            >
+              <Menu.Item key="7">Option sub 4</Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub5"
+              title={<span><Icon type="table" /><span>Administración de Clientes</span></span>}
+            >
+              <Menu.Item key="7">Option sub 5</Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub6"
+              title={<span><Icon type="area-chart" /><span>Estadisticas</span></span>}
+            >
+              <Menu.Item key="7">Option sub 6</Menu.Item>
+            </SubMenu>
+
+          </Menu>
+
+        </Sider>
+      </Layout>
     )
   }
 }
