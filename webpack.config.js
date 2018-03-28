03ada3f558b1b7177e44905c28f1f5a6b04043b2
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-03-28 13:12:10 -0600

    webpack rules changed

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.css$/, use: [ 
         {loader: 'style-loader'}, 
         {loader: 'css-loader', options: {url: false}} ] 
