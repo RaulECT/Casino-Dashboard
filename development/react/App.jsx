101bdedf2bbec7fadc6c9086617552c82b99ff33
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-03-28 13:20:42 -0600

    File RolesManagment.jsx created

--- development/react/App.jsx
+++ development/react/App.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
+import { HashRouter, Route } from 'react-router-dom'
+import RolesManagment from './pages/RolesManagment.jsx'
 
 class App extends Component {
   render() {
     return(
-      <div>
-        React base project
-      </div>
+      <HashRouter>
+        <div>
+          <Route exact path="/dashboard/gestion_roles" component = {RolesManagment} />
+        </div>
+      </HashRouter>
     )
   }
 }
---
+++ development/react/pages/RolesManagment.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class RolesManagment extends Component {
+
+}
+
+module.exports = RolesManagment
