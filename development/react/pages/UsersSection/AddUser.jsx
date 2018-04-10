65875f91693cd18d8c323e183370660d449d997d
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-04-10 10:21:35 -0500

    Add user styles fixed

--- development/react/pages/UsersSection/AddUser.jsx
+++ development/react/pages/UsersSection/AddUser.jsx
@@ -1,21 +1,137 @@
 import React, {Component} from 'react' 
 import {
   Form,
-  Modal
+  Modal,
+  Input,
+  DatePicker,
+  Row,
+  Col,
+  Select,
+  Radio
 } from 'antd'
+
+const FormItem = Form.Item
+const Option = Select.Option
+const RadioGroup = Radio.Group
 
 class AddUser extends Component {
 
+  constructor( props ) {
+    super( props )
+  }
+
   render() {
     const { visible, close } = this.props
+    const { getFieldDecorator } = this.props.form
+    
 
     return(
       <Modal
         title="Agregar usuario"
         visible={visible}
+        style={ {width: 1000} }
+        width={800}
         onCancel={close}
       >
+        <Form layout="inline">
+          
+          <FormItem
+            label="Nombre(s):"
+            className="add-user-form"
+            style={ {width: '70%'} }
+          >
+            {getFieldDecorator( 'name', { rules: [ {required: true, message: 'Ingrese un valor!'} ] } )(
+              <Input placeholder="Ej. Juan Perez" />
+            )}
+          </FormItem>
 
+          <FormItem
+            label="Apellido Paterno:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'firstName', { rules: [ {required: true, message: 'Ingrese un valor!'} ] } )(
+              <Input placeholder="Ej. Gonzales" />
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Apellido Materno:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'secondName', { rules: [ {required: true, message: 'Ingrese un valor!'} ] } )(
+              <Input placeholder="Ej. Gomez" />
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Nombre de usuario:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'userName', { rules: [ {required: true, message: 'Ingrese un valor!'} ] } )(
+              <Input />
+            )}
+          </FormItem>
+
+          <FormItem
+            label="E-mail:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'email', { rules: [ { type: 'email', message: 'No es un E-mail correcto!' }, {required: true, message: 'Ingrese un valor!'} ] } )(
+               <Input placeholder="correo@correo.com" />
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Contraseña:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'password', { rules: [ {required: true, message: 'Ingrese un Valor!'} ]} )(
+              <Input/>
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Repetir Contraseña:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'confirmPassword', { rules: [ {required: true, message: 'Ingrese un Valor!'} ] } )(
+              <Input />
+            )}
+   
+          </FormItem>
+
+          <FormItem
+            label="Fecha Nacimiento:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'birthday', { rules: [ {required: true, message: 'Ingrese un valor!'} ] } )(
+              <DatePicker placeholder="dd/mm/yyyy" />
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Rol:"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'role', { rules: [ {required: true, message: 'Seleccione un rol!'} ] } )(
+              <Select style={{ width: 120 }}>
+                <Option value="rol-1">Rol 1</Option>
+              </Select>
+            )}
+          </FormItem>
+
+          <FormItem
+            label="Genero"
+            className="add-user-form"
+          >
+            {getFieldDecorator( 'genere', { rules: [ {required: true, message: 'Seleccione una opción!'} ] } )(
+              <RadioGroup>
+                <Radio value={1}>Masculino</Radio>
+                <Radio value={2}>Femenino</Radio>
+              </RadioGroup>
+            )}
+          </FormItem>
+        </Form>
       </Modal>
     )
   }
