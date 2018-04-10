f5337fc8889873253cd0a9041d90f9f09a37f9f5
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-04-10 12:44:13 -0500

    EditUser.jsx file created

---
+++ development/react/pages/UsersSection/EditUser.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react' 
+import {
+  Form,
+  Modal,
+  Input,
+  DatePicker,
+  Select,
+  Button,
+  Popconfirm,
+  Radio
+} from 'antd'
+
+const FormItem = Form.Item
+const Option = Select.Option
+const RadioGroup = Radio.Group
+
+class EditUser extends Component {
+
+}
+
+module.exports = EditUser
