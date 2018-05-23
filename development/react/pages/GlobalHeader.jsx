bd547e65e06fdf1ca14a72f4f06cf224d9beff29
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-05-23 11:50:50 -0500

    GlobalHeader component documented

--- development/react/pages/GlobalHeader.jsx
+++ development/react/pages/GlobalHeader.jsx
@@ -1,3 +1,8 @@
+/**
+ * Componente que representa al Header general del Dashboard.
+ * @namespace GlobalHeader
+ * @extends Component
+ */
 import React, {Component} from 'react'
 import { Layout, Menu, Icon, Avatar, Divider, Modal, Dropdown } from 'antd'
 import './styles/configurations.css'
@@ -5,6 +10,11 @@ import './styles/configurations.css'
 const { Header, Sider, Content } = Layout
 
 class GlobalHeader extends Component {
+
+  /**
+   * Crea el componente.
+   * @param {object} props 
+   */
   constructor( props ) {
     super( props )
 
@@ -16,18 +26,28 @@ class GlobalHeader extends Component {
     this.showLogOutModal = this.showLogOutModal.bind( this )
   }
 
+  /**
+   * Cierrla sesión actual del usuario.
+   */
   logOut() {
     localStorage.removeItem("isLogin")
     localStorage.removeItem("token")
     this.props.history.push( '/' )
   }
 
+  /**
+   * Muestra el modal de confirmación para cerrar sesión.
+   */
   showLogOutModal() {
     this.setState( {
       isLogOutModalShowing: !this.state.isLogOutModalShowing
     } )
   }
 
+  /**
+   * Randeriza la vista del componente.
+   * @returns {string} HTML markup del componente.
+   */
   render() {
     let user = localStorage.user
     const {collapsed, toggle} = this.props
