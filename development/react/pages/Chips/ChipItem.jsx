6d37a321add406bd1346dc589fd36f34f2fcf45f
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-05-23 13:49:38 -0500

    ChipItem documented

--- development/react/pages/Chips/ChipItem.jsx
+++ development/react/pages/Chips/ChipItem.jsx
@@ -1,31 +1,35 @@
+/**
+ * Componente que representa a cada una de las fichas en el panel de ocnfiguración de fichas.
+ */
 import React, {Component} from 'react'
 import {Form, InputNumber} from 'antd'
 
 const FormItem = Form.Item
 
 class ChipItem extends Component {
-  constructor( props ) {
-    super( props )
-    
-  }
-  
+
+  /**
+   * Randeriza la vista del componente.
+   * @returns {string} HTML markup del componente.
+   */
   render() {
-    
+    const {img, chip, valueChange, avaible} = this.props
+
     return(
       <div className="chip-container">
-        <img className="chip-image" src={this.props.img} alt=""/>
+        <img className="chip-image" src={img} alt=""/>
         <FormItem
           label="Valor"
           className="chip-form"
         >
-          {this.props.fieldDecorator(this.props.chip, {
+          {this.props.fieldDecorator(chip, {
             rules: [{ required: true, message: 'Ingrese un valor!' }],
           })(
             <InputNumber 
               className="chip-input" 
               min={1} 
-              onChange={this.props.valueChange}
-              disabled={this.props.avaible}
+              onChange={valueChange}
+              disabled={avaible}
             />
           )}
         
