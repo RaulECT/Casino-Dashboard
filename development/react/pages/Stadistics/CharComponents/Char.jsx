3a11b71b8632a1019c4e17f7acdcd656caf77cfa
Raul Eduardo Canul Tuz <raul.canul@springlabs.net>
2018-06-07 10:22:12 -0500

    Char component created

---
+++ development/react/pages/Stadistics/CharComponents/Char.jsx
@@ -0,0 +1,78 @@
+import React, {Component} from 'react'
+import Chart from 'chart.js'
+import GraphicsManagment from '../../../controllers/GraphicsManagment'
+
+class Char extends Component {
+
+  constructor( props ) {
+    super( props )
+
+    this.graphicsManagment = new GraphicsManagment()
+  }
+
+  configBarChar( props ) {
+    const { xLabel, yLabel } = this.props
+    let barConfiguration = props
+    
+    barConfiguration[ 'xLabel' ] = xLabel
+    barConfiguration[ 'yLabel' ] = yLabel
+    barConfiguration[ 'type' ] = bar
+
+    return barConfiguration
+  }
+
+  configLineChar( props ) {
+    const { xLabel, yLabel } = this.props
+    let lineConfiguration = props
+    
+    lineConfiguration[ 'xLabel' ] = xLabel
+    lineConfiguration[ 'yLabel' ] = yLabel
+
+    return lineConfiguration
+  }
+
+  printChar() {
+    const { charType, data, dataLabels, chartLabel, title } = this.props
+    let charConfig = {}
+    let props = {
+      data,
+      dataLabels,
+      chartLabel,
+      title
+    }
+
+    switch ( charType ) {
+      case 'barGraph':
+        props = this.configBarChar( props )
+        charConfig = this.graphicsManagment.configBarGraphic( props )
+        break;
+
+      case 'pieGraph':
+        charConfig = this.graphicsManagment.configPieGraphic( props )
+        break;
+
+      case 'lineGraph':
+        props = this.configLineChar( props )
+        charConfig = this.graphicsManagment.configLineGraphic( props )
+        break;
+    
+      default:
+        props = this.configBarChar( props )
+        charConfig = this.graphicsManagment.configBarGraphic( props )
+        break;
+    }
+
+    const ctx = document.getElementById("myChart");
+    const char = new Chart( ctx, charConfig )
+  }
+
+  render() {
+    return(
+      <div id="chartContainer">
+        <canvas id="myChart" width="200" height="200"></canvas>
+      </div>
+    )
+  }
+}
+
+module.exports = Char
