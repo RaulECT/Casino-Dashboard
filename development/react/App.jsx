a1b3804557136da034bf72da08a81fe0a5296722
Raul Canul <eduardoct120@gmail.com>
2018-09-10 20:04:25 -0500

    Login component created

--- development/react/App.jsx
+++ development/react/App.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
+import { HashRouter, Route } from 'react-router-dom'
 
 import BingoGame from './containers/BingoGame'
+import Login from './containers/Login/Login'
 
 class App extends Component {
   render() {
     return(
-      <div>
-        <BingoGame />
-      </div>
+      <HashRouter>
+        <div>
+          <Route exact path="/" component={BingoGame} />
+          <Route exact path="/dashboard" component={Login} />
+          <Route exact path="/dashboard/login" component={Login} />
+        </div>
+      </HashRouter>
     )
   }
 }
--- development/react/containers/Login/Login.js
+++ development/react/containers/Login/Login.js
@@ -1,1 +1,67 @@
-impo
+import React, {Component} from 'react'
+import {
+  Form,
+  Icon,
+  Button,
+  Row,
+  Col,
+  Input
+} from 'antd'
+
+import './Login.css'
+
+const FormItem = Form.Item
+const styles = {
+  img: { width: '100%' },
+  icon: { color: 'rgba(0,0,0,.25)' }
+}
+
+class Login extends Component {
+
+  render() {
+    return(
+      <Row>
+        <Col
+          span={12}
+          offset={6}
+          xs={ { span: 24, offset: 0 } }
+        >
+          <div className="form-section">
+            <h1 className="login-title">Bingo Dashboard</h1>
+
+            <Form
+              className="login-form"
+            >
+              <FormItem>
+                <Input
+                  size="large"
+                  placeholder="E-mail"
+                  prefix={ <Icon type="user" style={ styles.icon } /> }
+                />
+              </FormItem>
+
+              <FormItem>
+                <Input 
+                  size="large"
+                  placeholder="Password"
+                  prefix={ <Icon type="lock" style={ styles.icon } /> }
+                />
+              </FormItem>
+
+              <FormItem>
+                <Button
+                  size="large"
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >Iniciar Sesión</Button>
+              </FormItem>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export default Login
