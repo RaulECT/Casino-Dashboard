65b8184c6bccdcc9a4d11154199094ce332f825b
RaulCT <raul.canul@springlabs.net>
2018-09-12 13:45:06 -0500

    getRandomCard function fixed

--- development/react/containers/BingoGame.js
+++ development/react/containers/BingoGame.js
@@ -40,12 +40,10 @@ class BingoGame extends Component {
 
   getRandomCard = () => {
     //TODO: REPLACE WITH RANDOM FUNCTION LATELY
-    const randomNumber = Math.floor(Math.random() * (this.props.cardList.length - 0 + 1)) + 0;
-   
+    const randomNumber = Math.floor(Math.random() * (this.props.cardList.length - 0 + 1)) + 0; 
     const randomCard = this.props.cardList[randomNumber]
-    const cardsUpdated = [...this.props.cardList]
+    const cardsUpdated = this.props.cardList.filter( ( element, index ) => index !== randomNumber  )
 
-    cardsUpdated.slice( randomNumber, 1 )
     this.props.onChangeCard( randomCard, cardsUpdated )
     this.playAudio( randomCard.audio )
   }
