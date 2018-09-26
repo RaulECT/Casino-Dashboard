86817ac534797d39dd223c295292621a28d0028c
RaulCT <raul.canul@springlabs.net>
2018-09-26 10:48:43 -0500

    random number fixed on generateRandomCard function

--- development/react/containers/GameControl/GameControl.js
+++ development/react/containers/GameControl/GameControl.js
@@ -45,10 +45,10 @@ class GameControl extends Component {
 
   generateRandomCard = () => {
     //TODO: REPLACE WITH RANDOM FUNCTION LATELY
-    const randomNumber = Math.floor(Math.random() * (this.props.cardList.length - 0 + 1)) + 0; 
+    const randomNumber = Math.floor(Math.random() * (this.props.cardList.length )) + 0; 
     const randomCard = this.props.cardList[randomNumber]
     const cardsUpdated = this.props.cardList.filter( ( element, index ) => index !== randomNumber  )
-
+    
     return { card: randomCard, cardList: cardsUpdated }
   }
 
