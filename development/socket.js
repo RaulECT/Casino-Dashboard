e7e548ab2006647405d5531bc38c14c53bdbf680
RaulCT <raul.canul@springlabs.net>
2018-11-15 16:33:39 -0600

    socket URL changed to test server

--- development/socket.js
+++ development/socket.js
@@ -3,8 +3,8 @@ import openSocket from 'socket.io-client'
 /**
  * CHANGE ON PRODUCTION SERVER
  */
-const socketURL = 'http://localhost:3000' // DEVELOPMENT
-//const socketURL = 'http://104.192.4.252:3000' // PRODUCTION
+//const socketURL = 'http://localhost:3000' // DEVELOPMENT
+const socketURL = 'http://104.192.4.252:3000' // PRODUCTION
 
 export const socket = openSocket( socketURL )
 
