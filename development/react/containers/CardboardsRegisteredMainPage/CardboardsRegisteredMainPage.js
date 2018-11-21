370ee1c6e95cde9c92747d0adb30ac03b4fc548b
RaulCT <raul.canul@springlabs.net>
2018-11-21 12:50:44 -0600

    CardboardsRegisteredList components files created

---
+++ development/react/containers/CardboardsRegisteredMainPage/CardboardsRegisteredMainPage.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+import Bakcground from '../../components/Background'
+
+class CardboardsRegisteredMainPage extends Component {
+
+  render() {
+    return (
+      <Bakcground >
+        Hola
+      </Bakcground>
+    )
+  }
+}
+
+export default CardboardsRegisteredMainPage
---
+++ development/react/containers/CradboardsRegisteredList/CradboardsRegisteredList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import CardboardsRegisteredMainPage from '../CardboardsRegisteredMainPage/CardboardsRegisteredMainPage'
+
+class CradboardsRegisteredList extends Component {
+
+  render() {
+    return (
+      <CradboardsRegisteredList />
+    )
+  }
+}
+
+export default CradboardsRegisteredList
