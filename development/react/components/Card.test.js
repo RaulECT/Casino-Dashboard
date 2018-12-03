36dee0cc86bbe20f28ca9b7c887c6df314649aeb
RaulCT <raul.canul@springlabs.net>
2018-12-03 13:18:03 -0600

    Card.test updated

--- development/react/components/Card.test.js
+++ development/react/components/Card.test.js
@@ -4,9 +4,24 @@ import React from 'react'
 import Card from './Card'
 
 describe( '<Card />', () => {
-  it( 'should render <Card /> component', () => {
-    const wrapper = shallow( <Card /> )
+  let wrapper
+  
+  beforeEach(() => {
+    wrapper = shallow( <Card /> )
+  })
 
+  it( 'should render <Card /> component', () => {
     expect( wrapper.length ).toEqual(1)
+  } )
+
+  it( 'should render <Card /> with default style', () => {
+    const defaultStyle = {
+      width: '200px',
+      height: '400px',
+      transform: 'scale(1)',
+    }
+    const cardStyles = shallow(<Card />).get(0).props.style
+   
+    expect( cardStyles ).toHaveProperty( 'transform', 'scale(1)' )
   } )
 } )
