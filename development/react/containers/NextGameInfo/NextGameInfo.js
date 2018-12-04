5b6d44693e3b7c1bb356f620bc340d854d1a7eb9
RaulCT <raul.canul@springlabs.net>
2018-12-04 13:08:51 -0600

    NextGameInfo tests developed

--- development/react/containers/NextGameInfo/NextGameInfo.js
+++ development/react/containers/NextGameInfo/NextGameInfo.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 
 import './NextGameInfo.css'
-import Chronometer from '../../components/Chronometer/Chronometer'
+import GameInfo from '../../components/GameInfo/GameInfo'
 
 class NextGameInfo extends Component {
   render() {
+    const component = this.props.game ? <GameInfo /> : <p className="next-game__error-msg">No hay siguiente partida registrada...</p>
     const style = {
       flex: this.props.flex ? this.props.flex : '0',
       background: this.props.opacity ? `rgba(0,0,0,${this.props.opacity})` : `rgba(0,0,0,0)`
@@ -15,20 +16,7 @@ class NextGameInfo extends Component {
         className="next-game__section" 
         style={style}
       >
-        <h1>Proxima Partida:</h1>
-
-        <span className="next-game__text-label">Tipo de Juego:</span>
-        <p className="next-game__game-type">DOBLE LINEA</p>
-
-        <span className="next-game__text-label">Precios de Inscripción:</span>
-        <ul className="next-game__cardboards-prices">
-          <li className="next-game__cardboards-prices--item">$10 Cartilla Simple</li>
-          <li className="next-game__cardboards-prices--item">$15 Cartilla Doble</li>
-          <li className="next-game__cardboards-prices--item">$20 Cartilla Triple</li>
-        </ul>
-
-        <span className="next-game__text-label">Tiempo de espera:</span>
-        <Chronometer />
+        { component }
       </div>
     )
   }
--- development/react/containers/NextGameInfo/NextGameInfo.test.js
+++ development/react/containers/NextGameInfo/NextGameInfo.test.js
@@ -2,6 +2,7 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 import NextGameInfo from './NextGameInfo'
+import GameInfo from '../../components/GameInfo/GameInfo'
 
 describe( '<NextGameInfo />', () => {
   let wrapper
@@ -13,4 +14,33 @@ describe( '<NextGameInfo />', () => {
   it( 'should render <NextGameInfo /> component', () => {
     expect( wrapper.length ).toEqual( 1 )
   } )
+
+  it( 'should render game not fount message when game is not passed as a prop', () => {
+    expect( wrapper.find( '.next-game__error-msg' ) ).toHaveLength( 1 )
+  } )
+
+  it( 'should render <GameInfo /> component when current game prop is setted', () => {
+    const gameFake = {
+      active: true,
+      createdBy: "85d556ca-096c-4283-8644-9db13d183c47",
+      createdOn: "2018-12-04T17:53:37.060Z",
+      doublePrice: 1500,
+      electronicPrice: 1000,
+      gameDate: "2018-12-04T20:52:14-06:00",
+      gameName: "Test_espera",
+      id: "fead1fdb-cfda-4578-8964-0fe0b15924aa",
+      lineConsPrize: 1200,
+      linePattern: "LINEA",
+      linePrize: 5000,
+      lotteryConsoPrize: 3400,
+      lotteryPattern: "DOBLE LINEA",
+      lotteryPrize: 15000,
+      singlePrice: 1000,  
+      triplePrice: 2000,
+    }
+
+    wrapper.setProps( { game: gameFake } )
+    expect( wrapper.find( GameInfo ) ).toHaveLength( 1 )
+  } )
+
 } )
