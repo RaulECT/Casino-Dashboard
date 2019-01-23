f931824a262bfdf80dc863fb67478fd703bc65af
RaulCT <raul.canul@springlabs.net>
2019-01-23 12:41:17 -0600

    Config change to production env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-module.exports = {
+let config = {
   CARDBOARDS_PER_PAGE: 154,
   // SOCKET_PRODUCTION_URL: 'http://104.192.4.252:3000',
-  SOCKET_PRODUCTION_URL: 'http://http://74.208.217.139:5003',
+  SOCKET_PRODUCTION_URL: 'http://74.208.217.139:5003',
   SOCKET_DEV_URL: 'http://localhost:5003',
   // APP_PORT: 3000,
   APP_PORT: 5003,
@@ -13,3 +13,20 @@ module.exports = {
   CARDBOARDS_URL: 'http://104.192.4.252:3001/#/',
   // CARDBOARDS_URL: 'http://104.192.4.252:3001/#/'
 }
+
+console.log('NODE_ENV: ', process.env.NODE_ENV)
+switch ( process.env.NODE_ENV ) {
+  case 'pruebasMID2':
+    config.SOCKET_PRODUCTION_URL = 'http://104.192.4.252:3000'
+    config.APP_PORT = 3000
+    break;
+
+  case 'sldevs':
+    config.SOCKET_PRODUCTION_URL = 'http://74.208.217.139:5003'
+    break
+
+  default:
+    break;
+}
+
+module.exports = config
